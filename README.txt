0964ab6f2df6d70b79d9bb3c71702965eec92abd
Priya Raghunatha <sripriya.raghunatha@yahoo.co.uk>
2017-11-01 12:26:35 +0000

    Changes to booking class

--- README.txt
+++ README.txt
@@ -11,4 +11,4 @@ Install chromedriver
 ====================
 
 You need to install chromedriver to run test on Google Chrome. 
-Then set chromedriver path in "BeforeClass method in src/main/java/com/hotelbooking/TestBooking.java.
+Then set chromedriver path using final variable CHROMEDRIVER_PATH of src/main/java/com/hotelbooking/Booking.java class.
--- src/main/java/com/hotelbooking/booking.java
+++ src/main/java/com/hotelbooking/booking.java
@@ -2,7 +2,6 @@ package com.hotelbooking;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,74 +11,75 @@ import org.testng.annotations.AfterClass;
 
 public class booking {
 
- private static WebDriver driver;
-  
-//  @BeforeClass
-//  public static void beforeClass() {
-//	  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-//	  driver = new ChromeDriver();
-//  }
+	private static WebDriver driver;
+	private static final String CHROMEDRIVER_PATH = "C:\\chromedriver.exe";
 
-  @AfterClass
-  public static void afterClass() {
-	  driver.quit();
-  }
-  
-  public static void createBooking(String firstname, String lastName, String price, Boolean depositPaid, String checkinDate, String checkoutDate) {
-	  setFirstName(firstname);
-	  setSurName(lastName);
-	  setPrice(price);
-	  setDeposit(depositPaid);
-	  setCheckinDate(checkinDate);
-	  setCheckoutDate(checkoutDate);
-	  driver.findElement(By.xpath("//*[@type='button' and @value=' Save ']")).click();
-  }
-  
-  public static void deleteBooking() {
-	  List<WebElement> buttonArrays = driver.findElements(By.xpath("//div[@id='bookings']//*[@type='button' and @value='Delete']"));
-	  System.out.println("No of Buttons: "+buttonArrays.size());
-	  for(int bindex=0;bindex<buttonArrays.size(); bindex++){
-		  System.out.println("Button: "+bindex);
-		  buttonArrays.get(bindex).click();
-	  }
-  }
-  
-  public static void deleteABooking(Integer bIndex) {
-	  List<WebElement> buttonArrays = driver.findElements(By.xpath("//div[@id='bookings']//*[@type='button' and @value='Delete']"));
-	  System.out.println("No of Buttons: "+buttonArrays.size());
-	  buttonArrays.get(bIndex).click();	  
-  }
-  
-  public static void getHotelReservationPage() {
-	  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-	  driver = new ChromeDriver();
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-      driver.get("http://hotel-test.equalexperts.io/");
-  }
-  
-  public static void setFirstName(String firstName){
-	driver.findElement(By.xpath("//*[@id='firstname']")).sendKeys(firstName);
-  }
-  
-  public static void setSurName(String surName){
-	driver.findElement(By.xpath("//*[@id='lastname']")).sendKeys(surName);
-  }
-  
-  public static void setPrice(String totalPrice){
-	driver.findElement(By.xpath("//*[@id='totalprice']")).sendKeys(totalPrice);
-  }
-  
-  public static void setDeposit(Boolean depositPaid){
-	Select paid = new Select(driver.findElement(By.xpath("//*[@id='depositpaid']")));
-			paid.selectByVisibleText(String.valueOf(depositPaid));
-  }
-  
-  public static void setCheckinDate(String checkinDate){
-	driver.findElement(By.xpath("//*[@id='checkin']")).sendKeys(checkinDate);
-  }
-  
-  public static void setCheckoutDate(String checkoutDate){
-	driver.findElement(By.xpath("//*[@id='checkout']")).sendKeys(checkoutDate);
-  }
+	//  @BeforeClass
+	//  public static void beforeClass() {
+	//	  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+	//	  driver = new ChromeDriver();
+	//  }
+
+	@AfterClass
+	public static void afterClass() {
+		driver.quit();
+	}
+
+	public static void createBooking(String firstname, String lastName, String price, Boolean depositPaid, String checkinDate, String checkoutDate) {
+		setFirstName(firstname);
+		setSurName(lastName);
+		setPrice(price);
+		setDeposit(depositPaid);
+		setCheckinDate(checkinDate);
+		setCheckoutDate(checkoutDate);
+		driver.findElement(By.xpath("//*[@type='button' and @value=' Save ']")).click();
+	}
+
+	public static void deleteBooking() {
+		List<WebElement> buttonArrays = driver.findElements(By.xpath("//div[@id='bookings']//*[@type='button' and @value='Delete']"));
+		System.out.println("No of Buttons: "+buttonArrays.size());
+		for(int bindex=0;bindex<buttonArrays.size(); bindex++){
+			System.out.println("Button: "+bindex);
+			buttonArrays.get(bindex).click();
+		}
+	}
+
+	public static void deleteABooking(Integer bIndex) {
+		List<WebElement> buttonArrays = driver.findElements(By.xpath("//div[@id='bookings']//*[@type='button' and @value='Delete']"));
+		System.out.println("No of Buttons: "+buttonArrays.size());
+		buttonArrays.get(bIndex).click();	  
+	}
+
+	public static void getHotelReservationPage() {
+		System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://hotel-test.equalexperts.io/");
+	}
+
+	public static void setFirstName(String firstName){
+		driver.findElement(By.xpath("//*[@id='firstname']")).sendKeys(firstName);
+	}
+
+	public static void setSurName(String surName){
+		driver.findElement(By.xpath("//*[@id='lastname']")).sendKeys(surName);
+	}
+
+	public static void setPrice(String totalPrice){
+		driver.findElement(By.xpath("//*[@id='totalprice']")).sendKeys(totalPrice);
+	}
+
+	public static void setDeposit(Boolean depositPaid){
+		Select paid = new Select(driver.findElement(By.xpath("//*[@id='depositpaid']")));
+		paid.selectByVisibleText(String.valueOf(depositPaid));
+	}
+
+	public static void setCheckinDate(String checkinDate){
+		driver.findElement(By.xpath("//*[@id='checkin']")).sendKeys(checkinDate);
+	}
+
+	public static void setCheckoutDate(String checkoutDate){
+		driver.findElement(By.xpath("//*[@id='checkout']")).sendKeys(checkoutDate);
+	}
 
 }
